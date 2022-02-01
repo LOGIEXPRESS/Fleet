f2652f17ebde4d4a83826e8c99dce8949ee18834
facundo2309 <facundoomargarcia@gmail.com>
2022-02-01 18:14:17 -0300

    rutas profile

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import  prueba  from "./prueba";
 import signUp from "./signup"
 import login from './login'
+import profileAdmin from './profiles'
 const router = Router();
 
 // router.use('/', dataFake)
@@ -13,5 +14,7 @@ router.use("/", prueba)
 router.use("/",signUp)
 
 router.use('/',login)
+
+router.use('/',profileAdmin)
 
 export default router;
