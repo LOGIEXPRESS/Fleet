b2ea170b0117e33f7f56c43a607e554a2cd32728
facundo2309 <facundoomargarcia@gmail.com>
2022-02-01 15:27:55 -0300

    action de login

--- clientnative/actions/index.js
+++ clientnative/actions/index.js
@@ -1,2 +1,26 @@
 import axios from "axios";
+import { API_URLS} from "@env"
 
+
+export function logiarUsuario(payload) {
+    return async function (dispatch) {
+      try {
+        console.log(API_URLS)
+        const response = await axios.post(`${API_URLS}/api/login`, payload)
+        .then((r) => {
+            dispatch({
+                type: "LOGEOO",
+                payload: r.data.payload,
+                token: r.data.token,
+            });
+            
+            console.log("Aqui esta el token llegando en la action logiarusuario:",r.data.token);
+         
+        });
+       
+      }catch(error){
+
+        console.log(error.response);
+      }
+    };
+  }
