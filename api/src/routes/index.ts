9e70eb48b1486f2974a942ad9825441411c55cc0
facundo2309 <facundoomargarcia@gmail.com>
2022-02-01 13:46:50 -0300

    first commit

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
-
 import  prueba  from "./prueba";
+import signUp from "./signup"
+import login from './login'
 const router = Router();
 
 // router.use('/', dataFake)
@@ -8,5 +9,9 @@ const router = Router();
 //router.use('/', UploadDataFake) 
 
 router.use("/", prueba)
+
+router.use("/",signUp)
+
+router.use('/',login)
 
 export default router;
