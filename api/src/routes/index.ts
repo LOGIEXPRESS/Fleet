dbc9c6231fcec0692f1ba0af2f69ddb6d88d52f6
Allan Torres <allaneduardot@gmail.com>
2022-02-02 17:34:14 -0400

    guardar flotas - Rutas travel - Sockets Travel - Aceptar viaje

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -3,11 +3,14 @@ import  prueba  from "./prueba";
 import signUp from "./signup"
 import login from './login'
 import profileAdmin from './profiles'
+import registerfleet from './registerfleet'
+import uploadDataFake from './uploadDataFake'
+import travel from './travel'
 const router = Router();
 
 // router.use('/', dataFake)
 
-//router.use('/', UploadDataFake) 
+ router.use('/', uploadDataFake) 
 
 router.use("/", prueba)
 
@@ -16,5 +19,7 @@ router.use("/",signUp)
 router.use('/',login)
 
 router.use('/',profileAdmin)
+router.use('/',registerfleet)
+router.use('/',travel)
 
 export default router;
