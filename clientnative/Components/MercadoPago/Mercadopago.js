4a9ac1796f1014d435d91bea8ef596d1665a2a77
Luis <rluis747@gmail.com>
2022-02-09 15:39:58 -0500

    Navigation in Mercadopago component

--- clientnative/Components/MercadoPago/Mercadopago.js
+++ clientnative/Components/MercadoPago/Mercadopago.js
@@ -1,8 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import axios from 'axios';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, ActivityIndicator } from 'react-native';
 import { useEffect, useState } from 'react';
  import { WebView } from 'react-native-webview'
+ import { useNavigation } from "@react-navigation/core";
 const FORM_ID = 'payment-form';
 
 import { API_URLS } from "@env"
@@ -10,56 +11,58 @@ import { API_URLS } from "@env"
 export default function Mercadopago() {
   const [link, setLink] = useState();
   const [render, setRender] = useState(null);
+  const navigation = useNavigation();
 
+  
+    
   const handleReset = () => {
     setLink("");
     setRender("");
+    navigation.navigate('ViewFleet')
   };
+
   const handlePago = () => {
     console.log("##### line 25 #######");
     axios
       .post(`${API_URLS}/api/mercadopago`, {
-        title: "Viaje 2",
+        title: "MercadoPago Fleet",
         unit_price: 2000,
         quantity: 1,
       })
       .then((res) => {
         console.log(res.data);
         setLink(res.data.init_points);
-        setId(res.data.response);
+        // setId(res.data.response);
       });
   };
 
+  useEffect(() => {
+    handlePago()
+}, [])
+
   return (
     <View style={styles.container}>
-      <Text>MercadoPago Fleet </Text>
-
+      
       {link ? (
-        <Text>
+          
+        <View style={{ flex: 1 }}>
+            <Text>MercadoPago Fleet </Text>
           {/*
         {  render
         ?<WebView source={{uri: link}}   style={{width:400, height:500}}/>
         :"Estamos procesando su pago..."
       } */}
-          <WebView source={{ uri: link }} style={{ width: 400, height: 500 }} />
-          {"\n"}
+          <WebView source={{ uri: link }} style={{ width: 350, height: 350 }} />
+          {/* {"\n"} */}
           <Button
             title="salir"
             onPress={handleReset}
             style={{ marginLeft: 300 }}
           />
-        </Text>
+        </View>
       ) : (
-        <Button title="Pagar" onPress={handlePago} />
+        <ActivityIndicator size="large" color="#0000ff" />
       )}
-
-      <Text
-        style={{
-          borderBottomColor: "black",
-          borderBottomWidth: 1,
-          width: 400,
-        }}
-      />
     </View>
   );
 }
