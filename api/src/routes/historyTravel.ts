1eb4a67aa072280459b7fbb639ac30513243609c
Macarena <macarenam328@gmail.com>
2022-02-09 21:38:29 -0300

    V9/2

--- api/src/routes/historyTravel.ts
+++ api/src/routes/historyTravel.ts
@@ -1,94 +1,94 @@
-// import { Response, Request, Router, NextFunction } from 'express';
+import { Response, Request, Router, NextFunction } from 'express';
 
-// import { Carrier } from '../models/Carrier';
-// import { Travel } from '../models/Travel';
-// import { Truck } from '../models/Truck';
-// import { Signup } from '../models/Signup';
-// // import { User } from '../models/User';
+import { Carrier } from '../models/Carrier';
+import { Travel } from '../models/Travel';
+import { Truck } from '../models/Truck';
+import { Signup } from '../models/Signup';
+// import { User } from '../models/User';
 
-// const router=Router()
+const router=Router()
 
 
 
-// router.get('/historyTravelUser/:idUserReg',async(req:Request,res:Response,next:NextFunction)=>{
+router.get('/historyTravelUser/:idUserReg',async(req:Request,res:Response,next:NextFunction)=>{
 
-//     let {idUserReg}=req.params
+    let {id}=req.params
 
-//     // console.log(req.params)
+    // console.log(req.params)
     
-//     try{
+    try{
 
-//         let user = await User.findOne({where:{idUserReg:idUserReg}})
+        let user = await Signup.findOne({where:{id:id}})
 
-//         if(!user){
-//             return res.json({menssage:'Not found user'})
-//         }
+        if(!user){
+            return res.json({menssage:'Not found user'})
+        }
 
-//         let travel=await Travel.findAll({
-//             where:{
-//                 userId:user.id,
+        let travel=await Travel.findAll({
+            where:{
+                adminId:user.id,
 
-//             },
-//             include:[{
-//                 model:Carrier
-//             }]
-//         })
+            },
+            include:[{
+                model:Carrier
+            }]
+        })
         
 
-//         if(travel.length>0){
+        if(travel.length>0){
             
-//             return res.json({menssage:'Found Travel',payload:travel})
-//         }else{
-//             return res.json({menssage:'Not found Travels'})
-//         }
+            return res.json({menssage:'Found Travel',payload:travel})
+        }else{
+            return res.json({menssage:'Not found Travels'})
+        }
 
-//     }catch(err){
-//         next(err)
-//     }
-
-
-
-// })
+    }catch(err){
+        next(err)
+    }
 
 
-// router.get(`/historyTravelCarrier/:idUser_Reg`,async(req:Request,res:Response,next:NextFunction)=>{
 
-//     let {idUser_Reg}=req.params
+})
 
-//     try{
-//         let user=await Carrier.findOne({where:{idUserReg:idUser_Reg}})//carrier 
 
-//         if(!user){
-//             return res.json({menssage:'Not found carrier'})
-//         }
+router.get(`/historyTravelCarrier/:idUser_Reg`,async(req:Request,res:Response,next:NextFunction)=>{
 
-//         let travel=await Travel.findAll({
-//             where:{
-//                 carrierId:user.id//el id del carrier 
-//             },
-//             include:[{
-//                 model:User
-//             }]
+    let {SignupId}=req.params
+
+    try{
+        let user=await Truck.findOne({where:{SignupId:SignupId}})//carrier 
+
+        if(!user){
+            return res.json({menssage:'Not found carrier'})
+        }
+
+        let travel=await Travel.findAll({
+            where:{
+                carrierId:user.id//el id del carrier 
+            },
+            include:[{
+                model:Signup
+            }]
         
-//         })
-//         if(travel.length>0){
+        })
+        if(travel.length>0){
             
-//             res.json({menssage:'Found Travel',payload:travel})
+            res.json({menssage:'Found Travel',payload:travel})
 
-//         }else{
+        }else{
             
            
-//             return res.json({menssage:'Not found Travels'})
-//         }
+            return res.json({menssage:'Not found Travels'})
+        }
 
-//     }catch(err){
-//         next(err)
-//     }
-
-
-
-// })
+    }catch(err){
+        next(err)
+    }
 
 
 
-// export default router
+})
+
+
+
+export default router
