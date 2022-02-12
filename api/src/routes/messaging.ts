5ef29ad2c33bfc00ad7cf3f6a22f975249bae1e0
Allan Torres <allaneduardot@gmail.com>
2022-02-12 10:49:11 -0400

    formato fecha

--- api/src/routes/messaging.ts
+++ api/src/routes/messaging.ts
@@ -2,19 +2,7 @@ import { Response, Request, Router, NextFunction } from 'express';
 import { Messaging } from '../models/Messaging';
 import { uuid } from 'uuidv4';
 const router=Router()
-const formatoDate=(cadena:any)=>{
-    let newCadena=cadena.toString().split(/[-,T,:]/);
-  //  let hora=newCadena[newCadena.length-3]+" "+newCadena[newCadena.length-2];
-    //let fecha=newCadena[2]+" "+newCadena[1]+" "+newCadena[0];
-    //let dateM=hora+" "+fecha;
-   //return newCadena[0]+':'+newCadena[1];
-    let sep=newCadena.toString().split(" ");
-    let fecha=sep[2]+"-"+sep[1]+"-"+sep[3];
-    let h=sep[4].replace(',',':');
-    let hora=h.substr(0,5);
- 
-    return fecha+" "+hora;
-    }
+
 router.post('/messaging',async(req: Request, res: Response) => {
      const {room,messageAdmin,messageCarrier} = req.body;
      //Messaging
@@ -36,25 +24,36 @@ router.post('/messaging',async(req: Request, res: Response) => {
     let message:any;
     
     let resp:any=[];
-    for(let i=0;i<messageX.length;i++){ 
-      if(messageX[i].messageCarrier){
-        author="Transportista";
-        message=messageX[i].messageCarrier;
+   if(messageX.length>0){
+    const formatoDate=(cadena:any)=>{
+        let newCadena=cadena.toString().split(/[-,T,:]/);
+      
+       let sep=newCadena.toString().split(" ");
+        let fecha=sep[2]+"-"+sep[1]+"-"+sep[3];
+        let h=sep[4].replace(',',':');
+        let hora=h.substr(0,5);
+     
+        return fecha+" "+hora;
         }
-        if(messageX[i].messageAdmin){
-        author="Administrador";
-        message=messageX[i].messageAdmin;
-        }
-        let fecha=formatoDate(messageX[i].createdAt);
-         resp[i]={
-          room:messageX[i].room,
-          author:author,
-          message:message,
-          time:fecha
-          
+            for(let i=0;i<messageX.length;i++){ 
+            if(messageX[i].messageCarrier){
+                author="Transportista";
+                message=messageX[i].messageCarrier;
+                }
+                if(messageX[i].messageAdmin){
+                author="Administrador";
+                message=messageX[i].messageAdmin;
+                }
+                let fecha=formatoDate(messageX[i].createdAt);
+                resp[i]={
+                room:messageX[i].room,
+                author:author,
+                message:message,
+                time:fecha
+                
+                }
         }
 }
-console.log(resp[0].time);
     res.send(resp);
 });
 export default router;	
