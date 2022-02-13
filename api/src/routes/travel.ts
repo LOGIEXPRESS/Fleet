7b4e83f1d1edb27c9df066c94b971c7972c5918c
Luis <rluis747@gmail.com>
2022-02-12 22:18:57 -0500

    Vamos a probar con id signup

--- api/src/routes/travel.ts
+++ api/src/routes/travel.ts
@@ -268,9 +268,9 @@ router.get('/alltraveltruck/:signupId', async (req: Request, res: Response, next
     }
   })
 
-  console.log("ESTO ES EN /alltraveltruck", { "travelinprocess":travelinprocess , "travelfinished": travelfinished } )
+  console.log("ESTO ES EN /alltraveltruck", { "travelinprocess":travelinprocess[0].id , "travelfinished": travelfinished } )
   
-  return res.status(200).json({ "travelinprocess":travelinprocess[0].id , "travelfinished": travelfinished });
+  return res.status(200).json({ "travelinprocess":travelinprocess , "travelfinished": travelfinished });
 
  }
  catch (err) {
