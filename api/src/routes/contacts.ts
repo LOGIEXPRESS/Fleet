0efc3186224af5b8d1704aa77089425383ec468f
Luis <rluis747@gmail.com>
2022-03-09 10:52:55 -0500

    REQ boy

--- api/src/routes/contacts.ts
+++ api/src/routes/contacts.ts
@@ -6,9 +6,11 @@ const router = Router()
 
 router.post('/contact',async (req:Request , res: Response, next: NextFunction) => {
     try {
-        const { name , email , description} = req.body
 
-        await Contacts.create({ name , email , description})
+        const { name , email , description} = req.body
+        console.log("REQ.Body", req.body);
+        
+        await Contacts.create({ name: name , email: email , description: description})
 
         res.json({menssage:'contact created'})
     } catch (error) {
